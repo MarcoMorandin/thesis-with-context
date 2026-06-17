282f2928cf97f38cbac8606f509c268c439387de
marcomorandin <emmeemmenove@gmail.com>
2026-06-17 16:47:51 +0200

    fix(baselines): remove unused import test that crashes time_vlm in uv environments

--- baselines/tier5/vendor/time_vlm/exp/exp_zero_shot_forecasting.py
+++ baselines/tier5/vendor/time_vlm/exp/exp_zero_shot_forecasting.py
@@ -1,5 +1,4 @@
-from patoolib import test_archive
-import test
+
 from data_provider.data_factory import data_provider
 from exp.exp_basic import Exp_Basic
 from utils.tools import EarlyStopping, adjust_learning_rate, visual
